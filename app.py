750ee55c28a514e0efef44783e1e2231a613a076
arnavgupta03 <arnavgupt03@gmail.com>
2021-01-16 10:22:14 -0500

    Added basic API functionality

--- app.py
+++ app.py
@@ -6,5 +6,57 @@ app = Flask(__name__)
 app.secret_key = config('ACCESS_SECRET')
 app.config.from_object('config')
 
+oauth = OAuth(app)
+oauth.register(
+    name='twitter',
+    api_base_url='https://api.twitter.com/1.1/',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    fetch_token=lambda: session.get('token'),
+)
+
+"""@app.errorhandler(OAuthError)
+def handle_error(error):
+    return render_template('error.html', error=error)"""
+
+@app.route('/')
+def homepage():
+    user = session.get('user')
+    return render_template('index.html', user=user)
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('authorize', _external=True)
+    return oauth.twitter.authorize_redirect(redirect_uri)
+
+@app.route('/authorize')
+def authorize():
+    token = oauth.twitter.authorize_access_token()
+    url = 'account/verify_credentials.json'
+    resp = oauth.twitter.get(url)
+    profile = resp.json()
+    session['token'] = token
+    session['user'] = profile
+    return redirect('/tweets')
+
+@app.route('/logout')
+def logout():
+    session.pop('token', None)
+    session.pop('user', None)
+    return redirect('/')
+
+@app.route('/tweets')
+def tweets():
+    url = 'statuses/home_timeline.json'
+    params = {'count': 50, 'trim_user': True, 'exclude_replies': True, 'include_entities': False}
+    prev_id = request.args.get('prev')
+    if prev_id:
+        params['max_id'] = prev_id
+
+    resp = oauth.twitter.get(url, params = params)
+    tweets = resp.json()
+    return render_template('tweets.html', tweets = tweets)
+
 if __name__ == '__main__':
     app.run(debug=True)
