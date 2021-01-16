822101796f82d2224bd5d85395961a1dbcd6cb43
arnavgupta03 <arnavgupt03@gmail.com>
2021-01-16 00:50:10 -0500

    Fix imports and initial config

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, redirect, request, url_for, session
+from decouple import config
+from authlib.integrations.flask_client import OAuth, OAuthError
+
+app = Flask(__name__)
+app.secret_key = config('ACCESS_SECRET')
+app.config.from_object('config')
---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+TWITTER_CLIENT_ID = os.getenv('API_KEY')
+TWITTER_CLIENT_SECRET = os.getenv('API_SECRET')
