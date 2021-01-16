82c4971ac4d1961cde7208bb7fb6f045cfed3dae
arnavgupta03 <arnavgupt03@gmail.com>
2021-01-16 13:13:44 -0500

    Add text generation with Markovify

---
+++ backend/markovscripts.py
@@ -0,0 +1,301 @@
+import markovify
+
+def chain(tweets):
+    text_model = markovify.NewlineText(tweets)
+
+    for i in range(5):
+        print(text_model.make_short_sentence(100))
+
+chain("""The climate crisis is here. We need a #GreenNewDeal.
+
+More than 20 years ago, Sergei Krikalev arrived at the @Space_Station with his crewmates to begin an unbroken streak of h…
+
+The filibuster is a Jim Crow relic that must be abolished.
+
+Imagine a world without Halloween. Or The Thing. Or Escape from New York. Or The Fog. Or Christine. Or Big Trouble in Litt…
+
+why doesn’t GINGER have an official vinyl release. that’s fucked up
+
+#WandaVision is #CertifiedFresh at 97% on the #Tomatometer, with 113 reviews.
+
+I recently found out that olive oil fraud is a thing - here's how it works.
+
+Mr. Starken teaches students with special needs in #PA. His students enjoy seeing their work displayed. ♥️ Whether they are w…
+
+ICYMI, peep this thread! Yes, @farai!!!
+
+.@farai looking great on @CrossConnection, sis! And thanks for all you do!
+
+And here’s realtor #JennaRyan showing her face and naming herself. She goes on to say that she is and her fellow te…
+
+Abolish the death penalty.
+
+@DavidBegnaud For someone to fly on a private jet to D.C to participate in the madness shows you these people aren’t the…
+
+ICYMI she committed a crime on Federal Property and FIVE people died, but she doesn’t think she should face any cha…
+
+Joe Biden has no clue what an MF DOOM is
+
+People start being 'concerned ' with where their tax money goes when they see poor and working class folks buy a crab leg…
+
+I love seeing @TiffanyDCross and @DrJasonJohnson in a split screen. I know it’s gonna be a good conversation!…
+
+why is it so expensive
+
+The Trump presidency is over in four days which is just six more weeks!
+
+Let me go start my breakfast over 😩
+
+You all realize that as each white supremacist seditious terrorist is arrested, they are campaigning for pardons fr…
+
+Excellent content for the people who don’t understand the whole Washington Football Team thing:
+
+Put him UNDER the jail! I’m glad they caught him! Dear God!
+
+Second time and it’s really peaceful this time different place in house!
+
+I just came here to get a whopper.
+
+Kinda funny that we call propaganda gaslighting now. Would be a lot funnier if like someone accused their shitty boyfrie…
+
+a s c e n d i n g
+
+u need to get some Ludwig von Bitches
+
+cc: @KellyannePolls
+
+Yea
+
+It has been hard to sit in one place all day during this pandemic while teaching. Can you imagine kids being tied to th…
+
+SugarFap
+
+John Waters just showed up on the latest ep of SVU playing the owner of a cam site and I GASPED
+
+‘Holiday’ is 97,000 views away from hitting 100 million. Stream now!
+
+I’ve done it before. I’m just waiting for @RuPaul to invite me back. 😘 I had the BEST time! And the Queens were as…
+
+how did Vince McMahon get away with like everything he’s done on fucking cable
+
+Dear @TiffanyDCross, thanks. You know why. 😏 @CrossConnection #CrossConnection #CrossTalk Okay, this. This is why…
+
+this is divine payment for the one peaceful year that we didn’t have any superhero shit
+
+Mozart in the Jungle was quite a good show, shame it got cancelled.
+
+Love this joke.
+
+normalise being on a bestie basis with ur teachers and co workers
+
+I do more for unifying this country when i consciously choose to use “mfs” instead of niggas in a tweet than anything a Dem…
+
+idk how but the Sony Marvel stuff just looks so much different (and better) visually than most MCU stuff. Visually…
+
+me n who🥺
+
+he is deranged
+
+Since we're talking about marvel tv today, i'd to like to remind y'all that this motherfucker is still the most evil an…
+
+Justice Sotomayor, on the Trump administration’s rush to kill.
+
+📢 Tout #FEESO membre en règle est encouragé à poser sa candidature à des postes à durée déterminée et des cooptatio…
+
+📢 Any #OSSTF member in good standing is encouraged to apply for one of our many provincial committees and councils…
+
+the best soundtrack from the best debut film of all time
+
+#Chartwell and #Extendicare don’t care about women. Exhibit: withholding pandemic pay from PSWs at Christmas⤵️
+
+This is how @TheRealEliGoree became Cassius Clay / Muhammad Ali for #OneNightInMiami.
+
+Where can you find @CommunityTV Cooperative Polygraphy?
+
+The POWER I felt leaving Birds of Prey... need that back 😭
+
+which is my main priority
+
+Premier @fordnation in a statement issued today: "I will not jeopardize a single Ontarian's life by ignoring public health…
+
+An observation on schools and COVID-19. In both Ontario and BC, there is what I will call a "magical unicorn" narrative relat…
+
+What is your “new thing” going to be for 2021?
+
+He has #swag
+
+On this date in 2014, Cooperative Polygraphy aired! Written by @atrubens and directed by Tristram Shapeero, a myste…
+
+My bf and I are going to Dunkin, y’all want anything?
+
+#SixSeasonsandaMARVELmovie why not?😋 @MarvelStudios @YNB @kenjeong #AndaMovie #SixSeasonsandaMovie #SixSeasonsandaLEGO…
+
+@YNB Yeeppp..I still have this old Mad Magazine
+
+Kinda perfect ngl. But oh well, still have like 10 years before I get married lol, not ruling anything out.
+
+Oval Office, Bush to Clinton, 1993: @kennerly
+
+This chick @KellyannePolls just said she’s proud that trump gave her a seat at the table. He didn’t listen to anyth…
+
+I agree.
+
+Was @KellyannePolls just on @billmaher carrying the water for the imp who incited a racist seditious terrorist riot…
+
+Reminder that you can't shame someone for their labor if their labor feeds your consumption
+
+”Plans to take carbon dioxide out of the air are becoming a giant loophole - just as experts have warned for years.”
+
+Her ignorance is a privilege :/
+
+❤️ Batwoman's Javicia Leslie is Gotham City's future |
+
+where is his oscar
+
+don't question it, it's 1am
+
+Who cares, money is good Get that bag king ❤️
+
+goodnight
+
+I can't wait until this next video is out so I can play Among Us with my discord server again without feeling guilty
+
+if your partner is a vampire and hasn’t bitten you yet despite you asking, then that is a red flag
+
+“Wonderwall is still a good song”, i say to rapturous booing
+
+The elderly father of a friend of mine was just diagnosed with COVID -- any prayers or good wishes are welcome on his behal…
+
+Looking at this, it certainly appears that Trump could be convicted.
+
+i am literally writing an adaptation it should be easy to put this onto the page but god apparently not
+
+In a break with recent tradition, no military farewell is being planned for Trump.
+
+One word: #Benghazi
+
+Four more days until we have a President. Which is fitting because we haven’t had a President in four years.
+
+Couple in Canada fined for breaking curfew rules after the woman was caught "walking" her husband on a leash, according to lo…
+
+when i’m screenwriting i’m going into Mank Mode
+
+BREAKING: Dallas TX area realtor Jenna Ryan has been arrested by the FBI for her role in the Capitol riot. How does feel?…
+
+We'll give this Limited Edition "Illusion" Nintendo Switch or $500 cash to a random person who retweets this tweet. To enter…
+
+Why in God's name is James Comey on my television. He's the very last person who would have any useful analysis about the…
+
+One of the great failures of civics education is I guess that we didn’t teach a generation of privileged white Americans what a…
+
+This is the last weekend of the Trump era.
+
+Wow. Kevin McCarthy’s mentor, former congressman Bill Thomas, excoriates Kevin for lying and hypocrisy. This is a must…
+
+I wonder if Kristen dunst wakes up declares ‘I’m THAT bitch in interview with a vampire’...
+
+you do what i do and let the breakup fill every waking moment of your life and headspace and watch your memories an…
+
+My biggest fear right here lol, coz I know that eventually the other person will either get completely bored of me…
+
+Paul Bettany hot
+
+I had a dream about Miami Vice. I should probably rewatch Miami Vice.
+
+fuuuuuuuuck. i hope this is real
+
+screenwriter voice this is the night that i get some writing done
+
+i miss when Don’t Hug Me I’m Scared was still a thing
+
+shut up i just cut all 3 of alison’s scenes from pyw to post here and she has a total of 9 minutes of screentime an…
+
+The fact that you're doing promoted tweets for your nicecore show with 'Time to start warming those biscuits' tells me you…
+
+WandaVision episode two is the first time since Iron Man that the MCU has even vaguely referenced two characters ha…
+
+Congrats, you can now afford to pay for a sit-down restaurant meal
+
+this rocks
+
+thought of a new name for a baby: Mctriple
+
+Illustrators: an easy thing I've done when I'm creatively drained is to literally just start my background canvas a…
+
+I unretweeted it but I can’t stop laughing at what’s in store for this lady
+
+stop i really wanna talk about alison in pyw but in one of her scenes she reveals a very important thing to cassie…
+
+alison brie nation when can i start posting ali’s scenes in pyw 🥺
+
+The wild thing about the biggest fans of capitalism is they have absolutely no idea how capitalism works
+
+little compilation of community behind the scenes :) i will probably make a part 2. god i love them so much
+
+imo WandaVision would be cool if it was just 9 straight episodes of riffing on different sitcoms instead of being t…
+
+alright watching Fate Of The Furious
+
+What went wrong with Cyberpunk 2077? Interviews with more than 20 current and former CD Projekt staff paint a complex pi…
+
+Give me Evan peters give me him please please pleaseeeee
+
+Wandavision would be better if it was just straight up like a real sitcom
+
+I have been thinking about wandavusion non stop since I watched it
+
+brb surprising my wife by learning the grubhub dance
+
+y’all who were calling this Lynchian... lol
+
+she
+
+I find it hilarious that Paul Bettany joined the MCU as just a AI voice and now he’s putting on a full costume/makeup and do…
+
+my votes for the hottest 100!
+
+thoughts on the blue light glasses?
+
+Back to back. Next up. My first Kiyoshi Kurosawa, the king of horror himself. #nw
+
+This is the dumbest thing I’ve ever made
+
+now listening
+
+my ex film student gf fucked a twin peaks fan and posted it in onlyfans im so fucked up — What’s the account https://t.…
+
+time to consoom the Marvel show
+
+My personal corner is coming together. I hanged my Cure (1997) poster, and I now have a shelf for my Blu-rays. https:/…
+
+🗣 HE'S 36 @KingJames
+
+George Conway discusses how support for President Trump is collapsing: "He’s going to be a pariah for the rest of his life." htt…
+
+Wait...the new president is taking...ACTION on something????
+
+Me too, I didn’t give any Capitol tours to anyone last week.
+
+Lauren Boebert goes completely off the deep end
+
+Is it too late to tweet “Yippi Kai Yay mask denier!” About Bruce Willis?
+
+Pelosi suggests criminal charges for any lawmaker who helped with Capitol riot
+
+the plan? tell everyone they will be getting $2000 if we win. after we win, we subtract the $600 they already got befo…
+
+Why did your wife leave you again? Oh yea, you sexually abused employees and fans. Just shut the fuck up.
+
+Still goes hard
+
+Justice Sotomayor opens her dissent in U.S. v. Dustin Higgs by saying the names of every person executed by the federal g…
+
+Ok, I will start. I didn’t give any Capitol tours to anyone last week.
+
+"$15 an hour? But that's as much as I make!"
+
+“His career average approval rating is the lowest for any president in modern polling, back to 1939, and he is the first pre…
+
+President-elect Biden announced he will use the Defense Production Act to maximize manufacturing of the coronavirus vaccine. Bo…
+
+I can just say anything and people will believe me. That’s how powerful I am """)
