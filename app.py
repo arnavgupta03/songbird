827d951c0fecbb853f989ba5e96a498ec018334c
arnavgupta03 <arnavgupt03@gmail.com>
2021-01-16 00:54:15 -0500

    Allow app to run

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ from authlib.integrations.flask_client import OAuth, OAuthError
 app = Flask(__name__)
 app.secret_key = config('ACCESS_SECRET')
 app.config.from_object('config')
+
+if __name__ == '__main__':
+    app.run(debug=True)
